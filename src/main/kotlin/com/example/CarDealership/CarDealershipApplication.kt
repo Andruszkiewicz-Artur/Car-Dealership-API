741c7683153291944cbd258396e403ebe5b6de60
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2024-11-12 21:20:22 +0100

    first commit

---
+++ src/main/kotlin/com/example/CarDealership/CarDealershipApplication.kt
@@ -0,0 +1,11 @@
+package com.example.CarDealership
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarDealershipApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarDealershipApplication>(*args)
+}
